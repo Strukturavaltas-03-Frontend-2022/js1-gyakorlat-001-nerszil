500078ca9852d2f340dc148af1940814e750f716
Szilárd Nérei <szilardnerei@gmail.com>
2022-08-14 18:29:35 +0200

    x

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const handleClick = () => {
+
+    let elements = document.querySelectorAll('.button');
+    for (let i = 0; i < elements.length; i ++ 1); {
+        elements[i]
+    }
+    return console.log('item: ' + elementIndex);
+}
+
+document.addEventListener("click", handleClick);
+
